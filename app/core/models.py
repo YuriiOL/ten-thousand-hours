e15ea6b5a09966f5a897de256d02311da781c95e
Yurii Oleksovskyi <yuoleksovskyi@echoua.com>
2025-07-16 22:37:32 +0200

    small correction

--- app/core/models.py
+++ app/core/models.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import (
 
 def timer_image_file_path(instance, filename):
     ext = os.path.splitext(filename)[1]
-    filename = f'{uuid.uuid4()}.{ext}'
+    filename = f'{uuid.uuid4()}{ext}'
 
     return os.path.join('uploads', 'timer', filename)
 
